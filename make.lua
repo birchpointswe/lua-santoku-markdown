167b262ca29c39efbcbe8c021a72406e4dc8e0ee
broma0 <121064585+broma0@users.noreply.github.com>
2026-08-10 20:08:49 -0400

    bump version to 2.0.0-1

--- make.lua
+++ make.lua
@@ -1,13 +1,13 @@
 local env = {
 
   name = "santoku-markdown",
-  version = "1.0.0-1",
+  version = "2.0.0-1",
   license = "MIT",
   public = true,
 
   dependencies = {
     "lua == 5.1",
-    "santoku >= 1.0.0, < 2.0.0",
+    "santoku >= 2.0.0, < 3.0.0",
   },
 
   test = {
