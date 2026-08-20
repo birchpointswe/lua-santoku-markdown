b9935d0d4703cfec47a5ffc274466ae169468dde
broma0 <121064585+broma0@users.noreply.github.com>
2026-08-20 19:09:02 -0400

    make.lua: add configure
    make.lua
      + configure

--- make.lua
+++ make.lua
@@ -1,3 +1,19 @@
+local fs = require("santoku.fs")
+local vendor = require("santoku.make.vendor")
+
+local vendored = {
+  {
+    file = "deps/sundown/sundown-37728fb2d7137ff7c37d0a474cb827a8d6d846d8.tar.gz",
+    url = "https://github.com/vmg/sundown/archive/37728fb2d7137ff7c37d0a474cb827a8d6d846d8.tar.gz",
+    sha256 = "1819a8f5725077e54586c426b93c5537f9b371dc0b9588ab776e909abf3e00ca",
+  },
+}
+
+local include = {}
+for i = 1, #vendored do
+  include[i] = vendored[i].file
+end
+
 local env = {
 
   name = "santoku-markdown",
@@ -5,12 +21,8 @@ local env = {
   license = "MIT",
   public = true,
 
-  vendor = {
-    {
-      file = "deps/sundown/sundown-37728fb2d7137ff7c37d0a474cb827a8d6d846d8.tar.gz",
-      url = "https://github.com/vmg/sundown/archive/37728fb2d7137ff7c37d0a474cb827a8d6d846d8.tar.gz",
-      sha256 = "1819a8f5725077e54586c426b93c5537f9b371dc0b9588ab776e909abf3e00ca",
-    },
+  rules = {
+    include = include,
   },
 
   dependencies = {
@@ -24,6 +36,16 @@ local env = {
     }
   },
 
+  configure = function (submake, envs)
+    for i = 1, #vendored do
+      local v = vendored[i]
+      local dest = fs.join(envs.root.build_dir, v.file)
+      submake.target({ dest }, { "make.lua" }, function ()
+        vendor.fetch(v, dest)
+      end)
+    end
+  end,
+
 }
 
 env.homepage = "https://github.com/birchpointswe/lua-" .. env.name
@@ -31,6 +53,6 @@ env.tarball = env.name .. "-" .. env.version .. ".tar.gz"
 env.download = env.homepage .. "/releases/download/" .. env.version .. "/" .. env.tarball
 
 return {
-  
+
   env = env,
 }
